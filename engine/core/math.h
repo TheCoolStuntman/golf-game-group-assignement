6cd73419b483c7a2b137d44d36e1d5010c6cbfbd
Fazil Khusnutdinov <khusnutdinov.fazil@gmail.com>
2025-05-19 15:44:14 +0200

    math is mathing

---
+++ engine/core/math.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <concepts>
+
+namespace Math {
+
+	template <typename T>
+	requires std::is_integral_v<T> || std::is_floating_point_v<T>
+	inline T clamp(T v, T min, T max) {
+		return (v < min ? min : (v > max ? max : v));
+	}
+
+}
